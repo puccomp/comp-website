698b63b913b3bf3259de9671c5a5a9ab416edaf2
thom <thom.nmaciel.baron@gmail.com>
2025-02-07 13:34:12 -0300

    fix: preview allowedHosts in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['puccomp.com.br'],
+  },
+  preview: {
+    allowedHosts: ['puccomp.com.br'], 
+  },
 })
